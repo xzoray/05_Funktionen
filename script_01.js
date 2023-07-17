49e0871b93e4ab1d5b58286ed18378bc133c1409
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-17 10:21:40 +0200

    function

--- script_01.js
+++ script_01.js
@@ -3,3 +3,13 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+
+function test()
+{
+    console.log("Hallo Sohrab!");
+}
